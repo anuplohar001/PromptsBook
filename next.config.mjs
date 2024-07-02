193c68a8b7de9216168f2c3cc60c72caa43c758b
anuplohar001 <anuplohar001@gmail.com>
2024-07-02 12:52:33 +0530

    Boundary

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,15 @@ const nextConfig = {
             topLevelAwait: true,
         }
         return config
-    }
+    },
+
+    
 }
+
+module.exports = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+}
+
 export default nextConfig
