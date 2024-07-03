9ee96d4a13a9828acfc521e83e8aeb3c3fd92da5
anuplohar001 <anuplohar001@gmail.com>
2024-07-03 10:01:16 +0530

    dynamic

--- app/api/feed/route.js
+++ app/api/feed/route.js
@@ -1,6 +1,8 @@
 import { connectDb } from "@utils/database";
 import Prompt from "@models/prompt";
 import { NextResponse } from "next/server";
+
+export const dynamic = 'force-dynamic';
 export const GET = async (req, res) => {
 
     try {
