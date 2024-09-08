b9a9e23f042bb9031a4ec2348caf47b5217f3c7b
anuplohar001 <anuplohar001@gmail.com>
2024-09-08 12:48:18 +0530

    Add Comment

--- app/comment/page.jsx
+++ app/comment/page.jsx
@@ -1,42 +1,47 @@
 "use client"
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
+
 import Comment from '@components/Comment'
 import { useSearchParams } from 'next/navigation'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 const page = () => {
-    const searchParams = useSearchParams()
-    const id = searchParams.get('id')
-    const [details, setdetails] = useState({})
-    const [user, setuser] = useState({})
-    const [pending, setPending] = useState(false)
-  
-    const getCard = async() => {
-        try{
-            setPending(true)
-            const response = await fetch(`/api/prompt/${id}/`, {method:"GET"})
-            
-            const data = await response.json()
-            setdetails(data)
-            setuser(data.padmin)
-            setPending(false)
-        }
-        catch{
-            console.log("Error")
-        }
-    }
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
+  const [details, setdetails] = useState({})
+  const [user, setuser] = useState({})
+  const [pending, setPending] = useState(false)
 
-    useEffect(() => {
-      getCard()
-    }, [id])
-    
+  const getCard = async () => {
+    try {
+      setPending(true)
+      const response = await fetch(`/api/prompt/${id}/`, { method: "GET" })
+
+      const data = await response.json()
+      setdetails(data)
+      setuser(data.padmin)
+      setPending(false)
+    }
+    catch {
+      console.log("Error")
+    }
+  }
+
+  useEffect(() => {
+    getCard()
+  }, [id])
+
 
   return (
-    <div>
-      {
-        pending ? (<Image src={'/assets/loader.svg'} height={20} width={20} alt='loading'/>): (<Comment postid = { id } userid = {details.padmin} prompt={details.prompt} tag={details.tag} username={user.username} img={user.image} email={user.email}/>)
-      } 
-    </div>
+    <Suspense>
+      <div>
+
+        {
+          pending ? (<Image src={'/assets/loader.svg'} height={20} width={20} alt='loading' />) : (<Comment postid={id} userid={details.padmin} prompt={details.prompt} tag={details.tag} username={user.username} img={user.image} email={user.email} />)
+        }
+      </div>
+    </Suspense>
   )
 }
 
