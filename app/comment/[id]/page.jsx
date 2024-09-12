4e3df4c6cf6880ca8f0fda9af8147e85ff58e3fb
anuplohar001 <anuplohar001@gmail.com>
2024-09-12 11:48:00 +0530

    Something wrong

--- app/comment/[id]/page.jsx
+++ app/comment/[id]/page.jsx
@@ -1,36 +1,24 @@
 
-import React, { Suspense } from 'react'
+import React from 'react'
 import Comment from '@components/Comment'
 import { checkEnvironment } from '@lib/actions'
 
-const Comments = async ({ params }) => {
+const page = async ({ params }) => {
 
   const response = await fetch(checkEnvironment().concat(`/api/prompt/${params?.id}/`), { method: "GET" })
   const data = await response.json()
 
-  if (!data) {
-    return (<div className='m-[15vw] text-red-600 font-bold text-lg'>Error in fetching Comments</div>)
-  }
+  if (!response.ok)
+    return (<div className='m-[10vw] text-red-600 font-bold'>Something Went Wrong !!!</div>)
 
   return (
 
     <div>
       {
         response.ok && (<Comment postid={params?.id} userid={data.padmin} prompt={data.prompt} tag={data.tag} username={data.padmin.name} img={data.padmin.image} email={data.padmin.email} />)
-      }
-      
+      }      
     </div>
   )
-}
-
-const page = ({params}) => {
-
-  return (
-    <Suspense>
-      <Comments params={params}/>
-    </Suspense>
-  )
-
 }
 
 export default page
--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,20 +1,23 @@
 //USER PROFILE
 import Profile from '@components/Profile'
 import { checkEnvironment } from '@lib/actions';
-
+function getrandom(count) {
+  return Math.floor(Math.random() * count)
+}
 const ProfileComp = async ({ params }) => {
-  
+
   const response = await fetch(checkEnvironment().concat(`/api/users/${params?.id}/posts`));
   const data = await response.json();
-
-
+  const random = getrandom(2)
+  if (!response.ok)
+    return (<div className='m-[10vw] text-red-600 font-bold'>Something Went Wrong !!!</div>)
 
   return (
     <div className='m-4 ml-5 mt-9'>
       {
         response.ok && (<Profile myPost={data} username={data[0].padmin.username} />)
       }
-      
+
     </div>
   )
 }
